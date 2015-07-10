21a44736867b458fe3adc738eddb2b0681aa059a
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-07-10 10:35:12 -0700

    Create function that returns an array without duplicate vals

--- arrayMods.js
+++ arrayMods.js
@@ -5,12 +5,23 @@ exports.arrDup = function (arr) {
       if (arr[i] === arr[j]) {
         return true
       }
-      else {
-        return false;
-      }
     }
   }
+  return false;
 }
+
+//Credit: I got the idea for this function from georg on stackoverflow:
+// http://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
+exports.unique = function(arr) {
+  var uniqueArr = {};
+  return arr.filter(function(item) {//Filter returns an array of values that pass through the provided function.
+    //The array is placed as an argument in the given function.
+    return uniqueArr.hasOwnProperty(item) ? false : (uniqueArr[item] = true);
+    /*If uniqueArr has a value that is the same as a value in the original array,
+    the value will not be pushed to the new array.*/
+  });
+};
 
 //arr = process.argv[2];
 //console.log(exports.arrDup(arr));
+//console.log(exports.unique(arr));
--- test/arrayMods-test.js
+++ test/arrayMods-test.js
@@ -4,5 +4,8 @@ var expect = require('chai').expect;
 describe('arrayMods.js', function() {
   it('will say true when passed [1, 2, 2, 3]', function() {
     expect(arrayMods.arrDup([1, 2, 2, 3])).to.eql(true);
-  })
-})
+  });
+  it('will say [1, 2, 3 , 4] when passed [1, 2, 2, 3, 4]', function() {
+    expect(arrayMods.unique([1, 2, 2, 3, 4])).to.eql([1, 2, 3, 4]);
+  });
+});
