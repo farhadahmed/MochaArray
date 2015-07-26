eab1197b59ece992f1b3a95f4abdd26368bf64ea
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-07-26 03:53:38 -0700

    Refactor tests

--- test/arrayMods-test.js
+++ test/arrayMods-test.js
@@ -2,25 +2,29 @@ var expect = require('chai').expect;
 var arrayMods = require('../arrayMods');
 
 describe('arrayMods.js', function() {
-  it('will say true when passed [1, 2, 2, 3]', function() {
+  it('arrdup says true when passed [1, 2, 2, 3]', function() {
     expect(arrayMods.arrDup([1, 2, 2, 3])).to.eql(true);
   });
-  it('will say [1, 2, 3 , 4] when passed [1, 2, 2, 3, 4]', function() {
+  it('arrdup says false when passed [1, 2, 3]', function() {
+    expect(arrayMods.arrDup([1, 2, 3])).to.eql(false);
+  });
+
+  it('unique says [1, 2, 3 , 4] when passed [1, 2, 2, 3, 4]', function() {
     expect(arrayMods.unique([1, 2, 2, 3, 4])).to.eql([1, 2, 3, 4]);
   });
-  it('will say ["foo", "bar"] when passed ["foo"], "bar"', function() {
-    expect(arrayMods.pushTo(['foo'], 'bar')).to.eql(['foo', 'bar']);
+  it('push says 2 when passed ["foo"], "bar"', function() {
+    expect(arrayMods.pushTo(['foo'], 'bar')).to.eql(2);
   });
-  it('will say ["foo"] when passed ["foo", "bar"]', function() {
-    expect(arrayMods.popFrom(['foo', 'bar'])).to.eql(['foo']);
+  it('pop says "bar" when passed ["foo", "foobar", "bar"]', function() {
+    expect(arrayMods.popFrom(['foo', 'foobar', 'bar'])).to.eql('bar');
   });
-  it('will say ["foo", "bar"] when passed ["foobar", "foo", "bar"]', function() {
-    expect(arrayMods.shiftFrom(['foobar', 'foo', 'bar'])).to.eql(['foo', 'bar']);
+  it('shift says "foobar" when passed ["foobar", "foo", "bar"]', function() {
+    expect(arrayMods.shiftFrom(['foobar', 'foo', 'bar'])).to.eql('foobar');
   });
-  it('will say ["foobar", "foo", "bar"] when passed ["foo", "bar"], "foobar"', function() {
-    expect(arrayMods.unshiftTo(['foo', 'bar'], 'foobar')).to.eql(['foobar', 'foo', 'bar']);
+  it('unshift says 3 when passed ["foo", "bar"], "foobar"', function() {
+    expect(arrayMods.unshiftTo(['foo', 'bar'], 'foobar')).to.eql(3);
   });
-  it('will say "d" when passed ["azr", "bc", "dddd"]', function() {
+  it('frequency2 says "d" when passed ["azr", "bc", "dddd"]', function() {
     expect(arrayMods.frequency2(['azr', 'bc', 'dddd'])).to.eql(['d']);
   })
 });
